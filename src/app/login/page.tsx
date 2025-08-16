b7e9a8bf4347879b66594de6e5555743bc31aaa3
edward-126 <rathnayaka3548@gmail.com>
2025-08-16 12:57:51 +0530

    feat/style: login/signup redone, zod and react-hook-form added for validation.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,8 +6,44 @@ import Swal from "sweetalert2";
 import { motion } from "framer-motion";
 import { FaGoogle, FaApple, FaFacebookF } from "react-icons/fa";
 
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+const formSchema = z.object({
+  username: z.string().min(1, {
+    message: "Username or email is required.",
+  }),
+  password: z.string().min(1, {
+    message: "Password is required.",
+  }),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
 const Page = () => {
   const router = useRouter();
+
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
   useEffect(() => {
     console.log("Checking session...");
 
@@ -38,23 +74,15 @@ const Page = () => {
       .catch((err) => {
         console.error("Fetch error:", err);
       });
-  }, []);
+  }, [router]);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const form = e.currentTarget;
-    const username = (form.elements.namedItem("username") as HTMLInputElement)
-      .value;
-    const password = (form.elements.namedItem("password") as HTMLInputElement)
-      .value;
-
+  const onSubmit = async (values: FormValues) => {
     try {
       const res = await fetch("http://localhost:5000/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include", // important to receive and send cookies automatically
-        body: JSON.stringify({ username, password }),
+        body: JSON.stringify(values),
       });
 
       const data = await res.json();
@@ -89,95 +117,104 @@ const Page = () => {
   };
 
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-10 px-4">
-      <motion.img
-        src="/assets/images/login/login-q-logo.png"
-        alt="Login Logo"
-        className="w-28 cursor-pointer rounded-xl"
-        whileHover={{ scale: 1.05 }}
+    <div className="flex min-h-dvh w-full flex-col items-center justify-center py-20">
+      <Image
+        height={80}
+        width={95}
+        src="/assets/branding/icon.svg"
+        alt="Queue.lk logo"
+        className="mb-10 size-24 cursor-pointer"
       />
 
-      <div className="w-full max-w-md rounded-xl bg-white">
-        <h2 className="mb-6 text-2xl font-bold">Login</h2>
+      <div className="w-full max-w-md">
+        <h3 className="mb-8">Login</h3>
 
-        <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
-          {/* Username */}
-          <div>
-            <label
-              htmlFor="username"
-              className="mb-2 block text-sm font-medium text-gray-700"
-            >
-              Username or Email
-            </label>
-            <input
-              id="username"
-              type="text"
-              placeholder="Enter your username or email"
-              className="focus:border-primary focus:ring-primary w-full rounded-md border border-gray-300 p-3 focus:outline-none focus:ring-2"
-              required
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username or Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your username or email"
+                      {...field}
+                      autoComplete="username"
+                    />
+                  </FormControl>
+                  <small className="text-muted-foreground">
+                    Enter your username or email
+                  </small>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-            <span className="mt-2 text-xs font-light">
-              Enter your username or email
-            </span>
-          </div>
 
-          {/* Password */}
-          <div>
-            <label
-              htmlFor="password"
-              className="mb-2 block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              placeholder="Enter your password"
-              className="focus:border-primary focus:ring-primary w-full rounded-md border border-gray-300 p-3 focus:outline-none focus:ring-2"
-              required
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter your password"
+                      {...field}
+                      autoComplete="current-password"
+                    />
+                  </FormControl>
+                  <small className="text-muted-foreground">
+                    Enter your password
+                  </small>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-            <span className="mt-2 text-xs font-light">Enter your password</span>
-          </div>
 
-          <div className="flex justify-end text-sm">
-            <a href="#" className="text-primary hover:underline">
-              Forgot password?
-            </a>
-          </div>
+            <div className="flex justify-end text-sm">
+              <Link href="#" className="underline">
+                <small className="!text-xs">Forgot password?</small>
+              </Link>
+            </div>
 
-          <button
-            type="submit"
-            className="bg-primary hover:bg-primary-dark rounded-md py-3 font-semibold transition"
-          >
-            Login
-          </button>
-        </form>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </form>
+        </Form>
 
         {/* Social login */}
-        <div className="my-6 flex items-center gap-2">
-          <hr className="flex-grow border-gray-300" />
-          <span className="whitespace-nowrap text-gray-500">Or login with</span>
-          <hr className="flex-grow border-gray-300" />
+        <div className="my-8 flex items-center gap-2">
+          <hr className="border-input flex-grow" />
+          <small className="text-muted-foreground whitespace-nowrap !text-xs">
+            Or login with
+          </small>
+          <hr className="border-input flex-grow" />
         </div>
 
-        <div className="mb-10 flex justify-center gap-6">
+        <div className="mb-10 flex justify-center gap-2">
           {[FaGoogle, FaApple, FaFacebookF].map((Icon, i) => (
             <button
               key={i}
               type="button"
-              className="flex w-full justify-center rounded-lg border border-gray-300 p-3 text-center transition-transform hover:scale-110"
+              className="border-input flex w-full cursor-not-allowed justify-center rounded-xl border p-3 text-center transition-transform hover:scale-[1.01]"
             >
-              <Icon className="text-black" size={24} />
+              <Icon className="size-5" />
             </button>
           ))}
         </div>
 
-        <p className="text-center">
-          Don’t have an account?{" "}
-          <a className="font-bold" href="/signup">
-            Sign up
-          </a>
-        </p>
+        <div className="w-full text-center">
+          <small className="!font-normal">
+            Don't have an account?{" "}
+            <a className="font-semibold" href="/signup">
+              Sign up
+            </a>
+          </small>
+        </div>
       </div>
     </div>
   );
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,38 +4,74 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { FaGoogle, FaApple, FaFacebookF } from "react-icons/fa";
 import Swal from "sweetalert2";
+import { useRouter } from "next/navigation";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import Image from "next/image";
+import Link from "next/link";
+
+const formSchema = z
+  .object({
+    username: z.string().min(3, {
+      message: "Username must be at least 3 characters.",
+    }),
+    email: z.string().email({
+      message: "Please enter a valid email address.",
+    }),
+    password: z.string().min(6, {
+      message: "Password must be at least 6 characters.",
+    }),
+    confirmPassword: z.string().min(1, {
+      message: "Please confirm your password.",
+    }),
+    termsAccepted: z.boolean().refine((value) => value === true, {
+      message: "You must accept the terms and conditions.",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"],
+  });
+
+type FormValues = z.infer<typeof formSchema>;
 
 const Page = () => {
-  const [conditionsSelected, setConditionsSelected] = useState(false);
+  const router = useRouter();
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      termsAccepted: false,
+    },
+  });
 
-    const form = e.currentTarget;
-    const username = (form.elements.namedItem("username") as HTMLInputElement)
-      .value;
-    const email = (form.elements.namedItem("useremail") as HTMLInputElement)
-      .value;
-    const password = (form.elements.namedItem("password") as HTMLInputElement)
-      .value;
-    const confirmPassword = (
-      form.elements.namedItem("confirm_password") as HTMLInputElement
-    ).value;
-
-    if (password !== confirmPassword) {
-      Swal.fire({
-        icon: "error",
-        title: "Validation Error",
-        text: "Passwords do not match.",
-      });
-      return; // stop submission if passwords don't match
-    }
-
+  const onSubmit = async (values: FormValues) => {
     try {
       const res = await fetch("http://localhost:5000/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({
+          username: values.username,
+          email: values.email,
+          password: values.password,
+        }),
       });
 
       const data = await res.json();
@@ -50,7 +86,7 @@ const Page = () => {
           showConfirmButton: false,
         });
 
-        window.location.href = "/login";
+        router.push("/login");
       } else {
         Swal.fire({
           icon: "error",
@@ -69,150 +105,164 @@ const Page = () => {
   };
 
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-10 px-4 py-10">
-      <motion.img
-        src="/assets/images/login/login-q-logo.png"
-        alt="Login Logo"
-        className="w-28 cursor-pointer rounded-xl"
-        whileHover={{ scale: 1.05 }}
+    <div className="flex min-h-dvh w-full flex-col items-center justify-center py-20">
+      <Image
+        height={80}
+        width={95}
+        src="/assets/branding/icon.svg"
+        alt="Queue.lk logo"
+        className="mb-10 size-24 cursor-pointer"
       />
 
-      <div className="w-full max-w-md rounded-xl bg-white">
-        <h2 className="mb-6 text-2xl font-bold">Sign up</h2>
+      <div className="w-full max-w-md">
+        <h3 className="mb-8">Sign up</h3>
 
-        <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
-          <div>
-            <label
-              htmlFor="username"
-              className="mb-2 block text-sm font-medium text-gray-700"
-            >
-              User Name
-            </label>
-            <input
-              id="username"
-              type="text"
-              placeholder="Enter your username or email"
-              className="focus:border-primary focus:ring-primary w-full rounded-md border border-gray-300 p-3 focus:outline-none focus:ring-2"
-              required
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your username"
+                      {...field}
+                      autoComplete="username"
+                    />
+                  </FormControl>
+                  <small className="text-muted-foreground">
+                    Enter your username
+                  </small>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-            <span className="mt-2 text-xs font-light">Enter your username</span>
-          </div>
-          <div>
-            <label
-              htmlFor="useremail"
-              className="mb-2 block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              id="useremail"
-              type="email"
-              placeholder="Enter your email"
-              className="focus:border-primary focus:ring-primary w-full rounded-md border border-gray-300 p-3 focus:outline-none focus:ring-2"
-              required
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Enter your email"
+                      {...field}
+                      autoComplete="email"
+                    />
+                  </FormControl>
+                  <small className="text-muted-foreground">
+                    Enter your email
+                  </small>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-            <span className="mt-2 text-xs font-light">Enter your email</span>
-          </div>
 
-          <div>
-            <label
-              htmlFor="password"
-              className="mb-2 block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-
-            <input
-              id="password"
-              type="password"
-              placeholder="Enter your password"
-              className="focus:border-primary focus:ring-primary w-full rounded-md border border-gray-300 p-3 focus:outline-none focus:ring-2"
-              required
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter your password"
+                      {...field}
+                      autoComplete="new-password"
+                    />
+                  </FormControl>
+                  <small className="text-muted-foreground">
+                    Enter your password
+                  </small>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-            <span className="mt-2 text-xs font-light">Enter your password</span>
-          </div>
 
-          <div>
-            <label
-              htmlFor="password"
-              className="mb-2 block text-sm font-medium text-gray-700"
-            >
-              Confirm Password
-            </label>
-
-            <input
-              id="confirm_password"
-              name="confirm_password"
-              type="password"
-              placeholder="Enter your password again"
-              className="focus:border-primary focus:ring-primary w-full rounded-md border border-gray-300 p-3 focus:outline-none focus:ring-2"
-              required
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter your password again"
+                      {...field}
+                      autoComplete="new-password"
+                    />
+                  </FormControl>
+                  <small className="text-muted-foreground">
+                    Confirm your password
+                  </small>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-            <span className="mt-2 text-xs font-light">Enter your password</span>
-          </div>
 
-          <div className="flex items-center justify-between gap-3 rounded-sm bg-red-100 p-3 text-sm">
-            <input
-              type="checkbox"
-              name="terms_checkbox"
-              id="terms_checkbox"
-              className="mt-0.5"
-              onClick={() => setConditionsSelected((prev) => !prev)}
+            <FormField
+              control={form.control}
+              name="termsAccepted"
+              render={({ field }) => (
+                <FormItem className="bg-primary/10 flex flex-row items-start space-x-1 space-y-0 rounded-sm p-3">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                      className="bg-background"
+                    />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormLabel className="text-primary-foreground text-sm leading-snug">
+                      You have read & accepted our terms & conditions.
+                    </FormLabel>
+                    <FormMessage />
+                  </div>
+                </FormItem>
+              )}
             />
-            <label htmlFor="terms_checkbox" className="leading-snug">
-              You have read & accepted our terms & conditions.
-            </label>
-          </div>
 
-          <button
-            type="submit"
-            disabled={!conditionsSelected}
-            className="bg-primary hover:bg-primary-dark rounded-md py-3 font-semibold transition disabled:cursor-not-allowed disabled:opacity-50"
-          >
-            Sign up
-          </button>
-        </form>
+            <Button type="submit" className="w-full">
+              Sign up
+            </Button>
+          </form>
+        </Form>
 
-        <div className="my-6 flex items-center gap-2">
-          <hr className="flex-grow border-gray-300" />
-          <span className="whitespace-nowrap text-gray-500">
+        {/* Social login */}
+        <div className="my-8 flex items-center gap-2">
+          <hr className="border-input flex-grow" />
+          <small className="text-muted-foreground whitespace-nowrap !text-xs">
             Or register with
-          </span>
-          <hr className="flex-grow border-gray-300" />
+          </small>
+          <hr className="border-input flex-grow" />
         </div>
 
-        <div className="mb-10 flex justify-center gap-6">
-          <button
-            type="button"
-            aria-label="Login with Google"
-            className="flex w-full justify-center rounded-lg border border-gray-300 p-3 text-center transition-transform hover:scale-110"
-          >
-            <FaGoogle className="text-black" size={24} />
-          </button>
-
-          <button
-            type="button"
-            aria-label="Login with Apple"
-            className="flex w-full justify-center rounded-lg border border-gray-300 p-3 transition-transform hover:scale-110"
-          >
-            <FaApple className="text-black" size={24} />
-          </button>
-
-          <button
-            type="button"
-            aria-label="Login with Facebook"
-            className="flex w-full justify-center rounded-lg border border-gray-300 p-3 transition-transform hover:scale-110"
-          >
-            <FaFacebookF className="text-black" size={24} />
-          </button>
+        <div className="mb-10 flex justify-center gap-2">
+          {[FaGoogle, FaApple, FaFacebookF].map((Icon, i) => (
+            <button
+              key={i}
+              type="button"
+              className="border-input flex w-full cursor-not-allowed justify-center rounded-xl border p-3 text-center transition-transform hover:scale-[1.01]"
+            >
+              <Icon className="size-5" />
+            </button>
+          ))}
         </div>
 
-        <p className="text-center">
-          Already have an account?
-          <a className="font-bold" href="/login">
-            Log in
-          </a>
-        </p>
+        <div className="w-full text-center">
+          <small className="!font-normal">
+            Already have an account?{" "}
+            <Link href="/login" className="font-semibold">
+              Log in
+            </Link>
+          </small>
+        </div>
       </div>
     </div>
   );
